50c70961d301ff8e182dbcf5a2b544d9257a1de2
FomichevAK-CMC <s02220265@gse.cs.msu.ru>
2025-05-12 00:46:00 +0300

    Update constants.py

--- in-dev/constants.py
+++ in-dev/constants.py
@@ -2,7 +2,6 @@ from typing import Union, List, Dict, Tuple
 
 type History = Union[Tuple[Dict], List[Dict]]
 
-model = r"C:\Utilities\AI\models\lmstudio-community\Meta-Llama-3-8B-Instruct-GGUF\Meta-Llama-3-8B-Instruct-Q8_0.gguf"
 CHATTING = "chatting"
 NEW_CONTEXT = "new_context"
 MAX_MESSAGE_LENGTH = 4096
